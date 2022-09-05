961f9b2db6018b3c987d5a1496e078a4bf18b7f6
Phiwa Madinga <phiwa@koedr.org>
2022-09-05 09:28:05 +0200

    added another modal

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal 
-    header='sign up for the giveaway'
-    text="Grab your swafg for a half price"
-  />
+  <br>
+
+  <p>Welcome</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme='sale' @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Open Modal</h1>
+      <p>This is an open modal</p>
+    </Modal>
+  </teleport>
+  <teleport to=".modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Modal 2 open</h1>
+      <p>This is another open modal</p>
+    </Modal>
+  </teleport>
+  <button @click.alt="toggleModal" >Open Modal (alt)</button>
+  <button @click="toggleModalTwo">Open second modal</button>
 </template>
 
 <script>
@@ -15,13 +36,17 @@ export default {
   },
   data(){
     return{
-      title: 'First Vue App'
+      title: 'First Vue App',
+      showModal: false,
+      showModalTwo: false,
     }
   },   
   methods: {
-    handleClick() {
-      console.log(this.$refs.name.value);
-      this.$refs.name.classList.add('active')
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo
     }
   }
 }
@@ -31,7 +56,7 @@ export default {
   *{
     margin: 0;
   }
-  #app {
+  #app, .modals {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
